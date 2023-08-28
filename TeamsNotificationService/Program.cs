93d5f0fe5eefb729dfb10150af9da5bd31ddf4f3
Ihor Reva <ihor.reva@precast.pl>
2023-08-28 21:22:38 +0200

    added policy retry

--- TeamsNotificationService/Program.cs
+++ TeamsNotificationService/Program.cs
@@ -4,19 +4,22 @@ using TeamsNotificationService.Services;
 using TeamsNotificationService.System;
 using TeamsNotificationService.System.Implementations;
 using TeamsNotificationService.Services.Implementations;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<IIOWrapper, IOWrapper>();
 builder.Services.AddScoped<IJsonWrapper, JsonWrapper>();
-builder.Services.AddHttpClient();
-builder.Services.AddScoped<IHttpWrapper, HttpWrapper>();
+builder.Services.AddHttpClient<IHttpWrapper, HttpWrapper>()
+    .AddTransientHttpErrorPolicy(policyBuilder => policyBuilder
+    .WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5)));
 builder.Services.AddScoped<IConfigurationService, NotificationConfigurationProvider>();
 builder.Services.AddScoped<INotificationSender, TeamsNotificationSender>();
 builder.Services.AddScoped<INotificator, Notificator>();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
